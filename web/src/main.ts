570cc9a42e44ef5a63620214cd50d0e53eaddc88
Kormos Áron <104300544+Kormos03@users.noreply.github.com>
2024-02-10 13:58:01 +0100

    a

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,5 +1,8 @@
-document.getElementById('sendButton')!.addEventListener('click', () => {
-    const email = ((document.getElementById('emailInput')!) as HTMLInputElement).value;
-    const password = ((document.getElementById('passwordInput')!) as HTMLInputElement).value;
-    console.log(email, password)
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('sendButton')!.addEventListener('click', () => {
+        const email = ((document.getElementById('emailInput')!) as HTMLInputElement).value;
+        const password = ((document.getElementById('passwordInput')!) as HTMLInputElement).value;
+        console.log("email, password")
+        document.getElementById('error')!.textContent = email;
+    })
 })
