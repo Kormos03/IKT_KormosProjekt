015f644a860d9684956ec7871836826e2cddb0dc
Kormos03 <aronkormos@gmail.com>
2024-04-21 22:51:37 +0200

    loginformAdmin cleaning DONE

--- frontend/frontend/src/Components/LoginFormAdmin.tsx
+++ frontend/frontend/src/Components/LoginFormAdmin.tsx
@@ -4,11 +4,12 @@ interface Props {
     onSuccessfulLogin: (token: string) => void;
 }
 
+const API_URL = "http://localhost:3000/auth/katus/admin/login";
+
 export function LoginFormAdmin({ onSuccessfulLogin }: Props) {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
     const [loginError, setLoginError] = useState('');
-    const [backendRoute, setBackendRoute] = useState("http://localhost:3000/auth/katus/admin/login");
 
     async function login(e: FormEvent) {
         e.preventDefault();
@@ -18,7 +19,7 @@ export function LoginFormAdmin({ onSuccessfulLogin }: Props) {
             password: pass,
         }
         
-        const response = await fetch(backendRoute, {
+        const response = await fetch(API_URL, {
             method: 'POST',
             headers: {
                 'Content-type': 'application/json',
@@ -26,38 +27,33 @@ export function LoginFormAdmin({ onSuccessfulLogin }: Props) {
             },
             body: JSON.stringify(loginData),
         });
-        console.log(response.status)
-        if (!response.ok) {
+
+        if (!response.ok || response.status === 500) {
             const errorObj = await response.json();
             setLoginError(errorObj.message);
             return;
         }
-        if(response.status === 500){
-            const errorObj = await response.json();
-            setLoginError(errorObj.message);
-            
-            return;
+
+        if(response.status === 201){
+            const tokenObj = await response.json();
+            localStorage.setItem('token', tokenObj.token);
+            onSuccessfulLogin(tokenObj.token);
         }
-        if(response.status == 201){
-        const tokenObj = await response.json();
-        localStorage.setItem('token', tokenObj.token);
-        console.log('Token:'+ localStorage.getItem('token'));
-        onSuccessfulLogin(tokenObj.token);
-    }
+
         setLoginError('');
         setEmail('');
         setPass('');
     }
 
-    return <div className="container">
-
-     <form onSubmit={login} className="login">
-     <h3>Bejelentkezés Admin</h3>
-        Email: <input type='email' onChange={e => setEmail(e.currentTarget.value)} />
-        Password: <input type='password' onChange={e => setPass(e.currentTarget.value)} /> <br />
-        <input className="btn btn-primary btn-md" type='submit' value='Login' />
-        <p>{loginError}</p>
-    </form>
-    </div>
+    return (
+        <div className="container">
+            <form onSubmit={login} className="login">
+                <h3>Bejelentkezés Admin</h3>
+                Email: <input type='email' onChange={e => setEmail(e.currentTarget.value)} />
+                Password: <input type='password' onChange={e => setPass(e.currentTarget.value)} /> <br />
+                <input className="btn btn-primary btn-md" type='submit' value='Login' />
+                <p>{loginError}</p>
+            </form>
+        </div>
+    );
 }
-
