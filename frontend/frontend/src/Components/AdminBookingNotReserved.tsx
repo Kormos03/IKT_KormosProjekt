3428a82a4bb9349ca7ee64d9da427f70fd6f7d9b
Kormos03 <aronkormos@gmail.com>
2024-04-21 21:13:04 +0200

    made the adminbookingnotreserved cleaner

--- frontend/frontend/src/Components/AdminBookingNotReserved.tsx
+++ frontend/frontend/src/Components/AdminBookingNotReserved.tsx
@@ -1,101 +1,99 @@
 import { useEffect, useState } from "react";
 import useAuth from "./useAuth";
-import { BookingModel } from "../BookingModel";
 import { useNavigate } from "react-router-dom";
-import { datesToReadableFormatFunc } from "./datesToReadableFormatFunc";
 import { groupBy } from 'lodash';
+import { datesToReadableFormatFunc } from "./datesToReadableFormatFunc";
+import { BookingModel } from "../BookingModel";
 import { renderGroupedBookings } from "./renderGroupedBookings";
+
+const API_URL = 'http://localhost:3000/booking/not_reserved/';
 
 export function AdminBookingNotReserved() {
     const { token } = useAuth();
-    const [open, setOpen] = useState({});
     const [bookingData, setBookingData] = useState([] as BookingModel[]);
-   const [checkedStates, setCheckedStates] = useState({});
+    const [checkedStates, setCheckedStates] = useState({});
+    const [isModalOpen, setIsModalOpen] = useState({});
 
-   const navigate = useNavigate();
+    const navigate = useNavigate();
 
-   const groupedBookings = groupBy(bookingData, booking => new Date(booking.dateStart).toDateString());
+    const groupedBookings = groupBy(bookingData, booking => new Date(booking.dateStart).toDateString());
 
-   const handleMasterCheckboxChange = (e : React.ChangeEvent<HTMLInputElement>) => {
-    const isChecked = e.target.checked;
-    const newCheckedStates: { [key: number]: boolean } = {};
-    bookingData.forEach((booking) => {
-        newCheckedStates[booking.id] = isChecked;
-    });
-    setCheckedStates(newCheckedStates);
-};
+    const handleMasterCheckboxChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+        const isChecked = e.target.checked;
+        const newCheckedStates: { [key: number]: boolean } = {};
+        bookingData.forEach((booking) => {
+            newCheckedStates[booking.id] = isChecked;
+        });
+        setCheckedStates(newCheckedStates);
+    };
+ 
+    const handleCheckboxChange = (bookingId : number, e : React.ChangeEvent<HTMLInputElement>) => {
+        setCheckedStates({
+            ...checkedStates,
+            [bookingId]: e.target.checked,
+        });
+    };  
 
-const handleCheckboxChange = (bookingId : number, e : React.ChangeEvent<HTMLInputElement>) => {
-    setCheckedStates({
-        ...checkedStates,
-        [bookingId]: e.target.checked,
-    });
-};
     useEffect(() => {
-        async function getAllNotReserved() {
-             const response = await fetch(`http://localhost:3000/booking/not_reserved/`, {
-                 method: 'GET',
-                 headers: {
-                     'Content-type': 'application/json',
-                     'Accept': 'application/json',
-                     'Authorization': `Bearer ${token}`
-                 },
-             });
-             if (!response.ok) {
-                 const errorObj = await response.json();
-                 console.log(errorObj);
-                 return;
-             }
-             const data = await response.json();
+        fetchAllNotReservedBookings();
+    }, []);
 
-            await setBookingData(data);
-         }
-
-        getAllNotReserved();
-    },  []);
-
-    //delete booking
-   async function deleteCheckedBookings(bookingIDOfButton: BookingModel) {
-        console.log(Object.keys(checkedStates).length)
-        if(Object.keys(checkedStates).length !== 0){
-        for(const bookingId in checkedStates){
-            const response = await fetch('http://localhost:3000/booking/not_reserved/' + bookingId, {
-                method: 'DELETE',
+    async function fetchAllNotReservedBookings() {
+        try {
+            const response = await fetch(API_URL, {
+                method: 'GET',
                 headers: {
                     'Content-type': 'application/json',
-                    'Accept': 'application/json',
+                    'Accept': 'application',
                     'Authorization': `Bearer ${token}`
                 },
             });
             if (!response.ok) {
-                const errorObj = await response.json();
-                console.log(errorObj);
-                return;
-            }}}
-            else{
-                const response = await fetch('http://localhost:3000/booking/not_reserved/' + bookingIDOfButton, {
-                    method: 'DELETE',
-                    headers: {
-                        'Content-type': 'application/json',
-                        'Accept': 'application/json',
-                        'Authorization': `Bearer ${token}`
-                    },
-                });
-                if (!response.ok) {
-                    const errorObj = await response.json();
-                    console.log(errorObj);
-                    return;
-                }}
-                navigate(0);
-}
-
-return (
-    <div className="container login">
-        <h1>Szabad időpontok kezelése</h1> Összes kijelölése <input type="checkbox" onChange={handleMasterCheckboxChange} />
-        {
-        //This function groups the bookings by date and displays them in a collapsible list
-            renderGroupedBookings(groupedBookings, open, setOpen, checkedStates, deleteCheckedBookings, handleCheckboxChange, datesToReadableFormatFunc, false)
+                throw new Error('Hiba történt a lekérdezés során');
+            }
+            const data = await response.json();
+            setBookingData(data);
+        } catch (error) {
+            console.error(error);
         }
-    </div>
-);
+    }
+
+    async function deleteCheckedBookings(bookingIDOfButton: BookingModel) {
+        try {
+            if(Object.keys(checkedStates).length !== 0){
+                for(const bookingId in checkedStates){
+                    await deleteBooking(bookingId);
+                }
+            } else {
+                await deleteBooking(bookingIDOfButton);
+            }
+            navigate(0);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function deleteBooking(bookingId: string | BookingModel) {
+        const response = await fetch(API_URL + bookingId, {
+            method: 'DELETE',
+            headers: {
+                'Content-type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+        });
+        if (!response.ok) {
+            throw new Error('Hiba történt a törlés során');
+        }
+    }
+
+    return (
+        <div className="container login">
+            <h1>Szabad időpontok kezelése</h1> 
+            Összes kijelölése <input type="checkbox" onChange={handleMasterCheckboxChange} />
+            {
+                renderGroupedBookings(groupedBookings, isModalOpen, setIsModalOpen, checkedStates, deleteCheckedBookings, handleCheckboxChange, datesToReadableFormatFunc, false)
+            }
+        </div>
+    )
 }
