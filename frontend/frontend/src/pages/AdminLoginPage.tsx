b666e567d9452de77076bcb910eef96561a1a5d9
Kormos03 <aronkormos@gmail.com>
2024-04-22 10:53:22 +0200

    adminloginpage cleaned

--- frontend/frontend/src/pages/AdminLoginPage.tsx
+++ frontend/frontend/src/pages/AdminLoginPage.tsx
@@ -3,30 +3,26 @@ import { LoginFormAdmin } from "../Components/LoginFormAdmin";
 import useAuthAdmin from "../Components/useAuthAdmin";
 
 export function AdminLoginPage() {
-  const { token, user,error, setToken, setUser, setError } = useAuthAdmin();;
-  const navigate = useNavigate();
+    const { token, user, error, setToken, setUser, setError } = useAuthAdmin();
+    const navigate = useNavigate();
 
-  function login(token: string) {
-    try{
-    setToken(token);
-    setUser(user);
-    localStorage.setItem('token', token);
-    console.log('Token:'+ localStorage.getItem('token'));
-    navigate('booking');
-    }
-    catch(error){
-      setError('Hiba történt a bejelentkezés során');
-    }
-    setError('');
+    function login(token: string) {
+        try {
+            setToken(token);
+            setUser(user);
+            localStorage.setItem('token', token);
+            navigate('booking');
+        } catch (error) {
+            setError('Hiba történt a bejelentkezés során');
+        }
+        setError('');
     }
 
-    return <div className="container login">
-    <Outlet/>
-    {
-      token ? null : <LoginFormAdmin onSuccessfulLogin={login}/> 
-    }        
-    {
-     <p>{error}</p>
-    }
-    </div >
+    return (
+        <div className="container login">
+            <Outlet />
+            {!token && <LoginFormAdmin onSuccessfulLogin={login} />}
+            {error && <p>{error}</p>}
+        </div>
+    );
 }
