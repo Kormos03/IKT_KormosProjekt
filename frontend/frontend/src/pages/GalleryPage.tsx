f258fa8849d0c8b3ff457fb75d5af69a048e3403
Kormos03 <aronkormos@gmail.com>
2024-04-22 11:27:28 +0200

    galleryPage cleaned

--- frontend/frontend/src/pages/GalleryPage.tsx
+++ frontend/frontend/src/pages/GalleryPage.tsx
@@ -8,22 +8,6 @@ const API_URL = 'http://localhost:3000/images/';
 export function GalleryPage() {
     const [gallery, setGallery] = useState([]);
 
-    async function fetchGallery() {
-        try {
-            const response = await fetch(API_URL, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json',
-                }
-            });
-            const data = await response.json();
-            setGallery(data);
-        } catch (error : any) {
-            throw new Error(error.message);
-        }
-    }
-
     useEffect(() => {
         async function fetchGallery() {
             try {
